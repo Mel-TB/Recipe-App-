c19d05ffd08f8da8366854f4456478041390de7c
Melinda T <melinda.trami@live.fr>
2023-04-16 17:56:42 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+
+export const KEY = "414589af-f161-4bcf-8d16-073c87e994ac";
+export const MODAL_CLOSE_SEC = 2.5;
+export const SERVINGS_TO_UPLOAD = 4;
+export const MS_PER_DAY = 1000 * 60 * 60 * 24;
+export const FIRST_MONDAY = new Date(0).getTime() + 4 * MS_PER_DAY;
